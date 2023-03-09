a7df2297203c14b25442b68cb521e8c3d722b71d
AleksandrVashchilo <suszycki.alexander@gmail.com>
2023-03-09 14:57:21 +0100

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,29 @@
+import javax.swing.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
+        //На входе массив из 10 элеентов
         ArrayListImplementation list = new ArrayListImplementation(10);
 
-        ArrayList<Integer> arrayList = new ArrayList<>(10);
-
         list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
         list.add(8);
-        list.add(20);
-        list.add(67);
+        list.add(9);
+        list.add(10);
+        list.print();
 
-        list.remove(2);
-        list.add(1, 888);
+
+        //Добавляем любо количество ээлементов
+        list.add(11);
+        list.add(12);
+
         list.print();
     }
 }
